ed073a69e98ccd68bc73f52cfdfa785e5ffc54f8
Illia <hetsko.ilya@gmail.com>
2025-03-18 03:08:26 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const images = document.getElementById('thumbs');
+
+images.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName === 'IMG') {
+    const link = e.target.closest('a');
+
+    if (link) {
+      mainImage.src = link.href;
+    }
+  }
+});
